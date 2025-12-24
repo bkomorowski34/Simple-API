ac0c0a02f9c46ce7e7973327c093725e073c42b7
Bartek <46711110+bartekkomorowski34@users.noreply.github.com>
2025-12-24 23:43:51 +0100

    create db and conn

--- app/app.py
+++ app/app.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI, HTTPException
-from app.schemas import PostCreate
+from app.schemas import PostCreate, PostResponse
+from app.db import Post, create_db_and_tables, get_async_session
+from sqlalchemy.ext.asyncio import AsyncSession
+from contextlib import asynccontextmanager
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await create_db_and_tables()
+    yield
+
+app = FastAPI(lifespan=lifespan)
 
 text_posts = {
     1: {"title": "Getting Started", "content": "This is a simple test post created for API development."},
@@ -23,14 +31,14 @@ def get_all_posts(limit: int = None):
     return text_posts
 
 @app.get("/posts/{id}")
-def get_post(id: int):
+def get_post(id: int) -> PostResponse:
     if id not in text_posts:
         raise HTTPException(status_code=404, detail="Post not found")   
 
     return text_posts.get(id)
 
 @app.post("/posts")
-def create_post(post: PostCreate):
+def create_post(post: PostCreate) -> PostResponse:
     new_post = {"title": post.title, "content": post.content}
     text_posts[max(text_posts.keys()) + 1] = new_post
     return new_post
